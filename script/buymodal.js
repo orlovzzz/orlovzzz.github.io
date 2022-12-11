00f1ac72af3c52da47f50acdedb8fcac7b4b8dbe
Artem <artem@gmail.com>
2022-12-11 23:04:09 +0300

    add buymodal.js

---
+++ script/buymodal.js
@@ -0,0 +1,63 @@
+var sam = document.getElementById("sam");
+var dost = document.getElementById("dost");
+
+sam.addEventListener("click", function() {
+    sam.style.background = "black";
+    sam.style.color = "white";
+    dost.style.background = "white";
+    dost.style.color = "black";
+    document.getElementById("sel").style.display = "block";
+    document.getElementById("inps").style.display = "none";
+});
+
+dost.addEventListener("click", function() {
+    sam.style.background = "white";
+    sam.style.color = "black";
+    dost.style.background = "black";
+    dost.style.color = "white";
+    document.getElementById("sel").style.display = "none";
+    document.getElementById("inps").style.display = "block";
+    document.getElementById("modtext").innerHTML = "";
+    document.getElementById("modtext").style.opacity = "0";
+});
+
+
+var mas = [];
+for (var i = 0; i < 5; i++) {
+    var x = (Math.floor(Math.random() * 6));
+    if (x == 0) {
+        mas.push("Товара нет в наличии. Попробуйте выбрать другой магазин.")
+    }
+    else {
+        mas.push("Товар в наличии " + x + " шт.");
+    }
+}
+
+document.getElementById("sel").addEventListener("click", function() {
+    console.log(document.getElementById("sel").value)
+    var x = (Math.floor(Math.random() * 6));
+    document.getElementById("modtext").style.opacity = "1";
+    document.getElementById("modtext").innerHTML = mas[document.getElementById("sel").value];
+    if (mas[document.getElementById("sel").value].length == 56) {
+        document.getElementById("modtext").style.color = "red";
+    }
+    else {
+        document.getElementById("modtext").style.color = "green";
+    }
+});
+
+function closeWindow() {
+    document.getElementById("fon").style.display = "none";
+    document.getElementById("buymodal").style.display = "none";
+}
+
+document.getElementById("close").addEventListener("click", function() {
+    closeWindow()
+});
+
+
+
+document.getElementById("oform").addEventListener("click", function() {
+    document.getElementById('zakaz').style.opacity = "1";
+    setTimeout(closeWindow, 1000);
+})
--- script/katalog.js
+++ script/katalog.js
@@ -1,17 +1,27 @@
 var blocks = document.querySelectorAll(".manblock")
 blocks.forEach(function(block) {
-    block.addEventListener("click", function() {
-        if (block.style.height == '350px') {
-            block.style.height = "300px";
-            block.querySelector("#buy").style.opacity = "0";
+    block.addEventListener("click", e => {
+        if (block.style.height == '490px') {
+            if (e.target.tagName == "BUTTON") {
+                e.target.addEventListener("click", function() {
+                    document.getElementById("fon").style.display = "block";
+                    document.getElementById("buymodal").style.display = "block";
+                });
+            } else {
+                block.style.height = "300px";
+                block.querySelector("#buy").style.opacity = "0";
+                block.querySelector("#opis").style.opacity = "0";
+            }   
         } else {
             for (var i = 0; i < blocks.length; i++) {
                 if (blocks[i] == block) {
-                    blocks[i].style.height = "350px";
+                    blocks[i].style.height = "490px";
                     blocks[i].querySelector("#buy").style.opacity = "1";
+                    blocks[i].querySelector("#opis").style.opacity = "1";
                 } else {
                     blocks[i].style.height = "300px";
                     blocks[i].querySelector("#buy").style.opacity = "0";
+                    blocks[i].querySelector("#opis").style.opacity = "0";
                 }
             }
         }
