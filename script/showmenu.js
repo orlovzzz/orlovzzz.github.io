2e12628797dfe8b12e184b21bc2e39f8109d25a8
Artem <artem@gmail.com>
2022-12-11 16:46:36 +0300

    initial

--- script/showmenu.js
+++ script/showmenu.js
@@ -1,9 +1,11 @@
 document.getElementById("menu").addEventListener("click", function() {
     var x = document.getElementById("blackmenu");
-    x.style.display = "grid";
     if (x.style.opacity == "1") {
         x.style.opacity = "0";
+        x.style.display = "none";
+        
     } else {
+        x.style.display = "grid";
         x.style.opacity = "1";
     }
 })
